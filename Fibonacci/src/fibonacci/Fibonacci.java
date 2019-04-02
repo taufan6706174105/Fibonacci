baba45e6872736a032c1f354f103b079d085421b
Taufan Akbar Aprialdo Daulay <taufanaprialdo@student.telkomuniversity.ac.id>
2019-04-02 08:37:26 +0700

    Add files via upload

---
+++ Fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,29 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {  
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int nilai;
+        
+        System.out.println("Input:");
+        nilai = scan.nextInt();
+        ++nilai;
+        long fibo[] = new long[nilai];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        System.out.println("Output:");
+        for (int i = 2; i < nilai; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+        for (int i = 1; i < nilai; i++) {
+            System.out.print(fibo[i] + " ");
+        }
+        
+    }   
+    
+}
